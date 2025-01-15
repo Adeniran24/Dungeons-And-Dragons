417df116ea60e00f549cb0027e13a3bc41b01c43
Adeniran Ádám Kristóf <adenirana@kkszki.hu>
2025-01-15 11:07:10 +0100

    Update Connect.php

--- connect.php
+++ connect.php
@@ -10,9 +10,7 @@ $conn = mysqli_connect($dbserver, $dbusername, $dbpassword, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-else {
-    echo "Connected successfully";
-}
+
 
 
 
